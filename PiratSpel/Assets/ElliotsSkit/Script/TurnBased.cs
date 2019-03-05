bddba056079566933df5e1babe8b99139d678fe3
UltraBose13 <47039692+UltraBose13@users.noreply.github.com>
2019-03-05 09:06:47 +0100

    Turnbased.

---
+++ PiratSpel/Assets/ElliotsSkit/Script/TurnBased.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnBased : MonoBehaviour
+{
+
+    public static TurnBased Instance { set; get; }
+    public GameObject[] Players;
+    bool[] allowedMoves { set; get; }
+
+    Vector3 v3out = Vector3.zero;
+ 
+
+    public static bool Player1Turn = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnMouseDown()
+    {
+        if(Players.Length == 2)
+        {
+            if (Player1Turn)
+            {
+                Players[0].GetComponent<MeshRenderer>().material.color = Color.red;
+                Players[1].GetComponent<MeshRenderer>().material.color = Color.white;
+                Player1Turn = false;
+            }
+            else
+            {
+                Players[1].GetComponent<MeshRenderer>().material.color = Color.red;
+                Players[0].GetComponent<MeshRenderer>().material.color = Color.white;
+                Player1Turn = true;
+            }
+
+            Debug.Log("Mouse is over GameObject. " + gameObject.name);
+        }
+    }
+}
