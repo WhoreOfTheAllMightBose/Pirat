cd7a744aa50d5b7b3dc9ca23dd6189e500ef6104
UltraBose13 <47039692+UltraBose13@users.noreply.github.com>
2019-03-06 10:51:59 +0100

    turn/början på att lägga ut kort på highligts.
    bugg med att du kan sätta ut på vilken highligt som helst./dem förs´vinner inte.

---
+++ PiratSpel/Assets/ElliotsSkit/Script/TemporaryCard.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemporaryCard : MonoBehaviour
+{
+    public bool IsSelected;
+    public GameObject tempCard;
+    public Vector3[] PossibleMoves;
+    Vector3 restartpos;
+
+
+    public int CurrentX { set; get; }
+    public int CurrentY { set; get; }
+    public bool IsPlayer1;
+    bool isDown = false;
+    bool isOver;
+
+    void Start()
+    {
+    }
+    //int CurrentXMax;
+    //int CurrentXMin;
+    //int CurrentYMax;
+    //int CurrentYMin;
+
+    private void Update()
+    {
+        if (IsSelected)
+        {
+            //  Vector3 mousepos = new Vector3(Input.mousePosition.x, 0, Input.mousePosition.z);
+            //transform.position = Camera.main.WorldToScreenPoint(mousepos) * Time.deltaTime;
+            // transform.position = Vector3.Lerp(transform.position, mousepos, 1 * Time.deltaTime);
+            Vector3 mouse = Input.mousePosition;
+            Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+            RaycastHit hit;
+            RaycastHit hitD;
+            if (!isDown)
+            {
+                if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
+                {
+                    // print(Input.mousePosition);
+                    print(hit.transform.name);
+                    Vector3 pos = new Vector3(hit.point.x, 1, hit.point.z);
+                    transform.position = pos;
+                }
+            }
+            if (Physics.Raycast(transform.position, Vector3.down, out hitD))
+            {
+                isOver = true;
+
+                if (hitD.transform.tag == "slot")
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        isDown = true;
+                        print(hitD.transform.name);
+                        transform.position = hitD.transform.position;
+                        hitD.collider.GetComponent<slots>().isFree = false;
+                    }
+                }
+            }
+        }
+    }
+
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if(other.tag == "slot")
+    //    {
+    //        transform.position = other.transform.position;
+    //    }
+    //}
+
+    private void OnMouseDown()
+    {
+        CurrentX = (int)Input.mousePosition.x;
+        CurrentY = (int)Input.mousePosition.y;
+        if (!isOver)
+        {
+            if (TurnBased.Player1Turn && IsPlayer1)
+            {
+                IsSelected = !IsSelected;
+
+                GameObject g = GameObject.FindGameObjectWithTag("Player1slots");
+
+                if (IsSelected)
+                {
+                    restartpos = transform.position;
+                    for (int i = 0; i < 4; i++)
+                    {
+                        g.transform.GetChild(i).gameObject.SetActive(true);
+                    }
+                }
+                else
+                {
+                    transform.position = restartpos;
+                    for (int i = 0; i < 4; i++)
+                    {
+                        g.transform.GetChild(i).gameObject.SetActive(false);
+                    }
+                }
+            }
+            else if (!TurnBased.Player1Turn && !IsPlayer1)
+            {
+
+                IsSelected = !IsSelected;
+
+                GameObject g = GameObject.FindGameObjectWithTag("Player2slots");
+
+                if (IsSelected)
+                {
+                    restartpos = transform.position;
+                    for (int i = 0; i < 4; i++)
+                    {
+                        g.transform.GetChild(i).gameObject.SetActive(true);
+                    }
+                }
+                else
+                {
+                    print(restartpos);
+                    transform.position = restartpos;
+                    for (int i = 0; i < 4; i++)
+                    {
+                        g.transform.GetChild(i).gameObject.SetActive(false);
+                    }
+                }
+            }
+        }
+    }
+
+    public void SetPosition(int x, int y)
+    {
+            CurrentY = y;
+            CurrentX = x;
+    }
+
+    public virtual bool[,] PossibleMove()
+    {
+        return new bool[4,2]; // 4 = xles 2 = yled
+    }
+}
--- PiratSpel/Assets/ElliotsSkit/Script/TurnBased.cs
+++ PiratSpel/Assets/ElliotsSkit/Script/TurnBased.cs
@@ -7,10 +7,13 @@ public class TurnBased : MonoBehaviour
 
     public static TurnBased Instance { set; get; }
     public GameObject[] Players;
-    bool[] allowedMoves { set; get; }
+    public bool[,] allowedMoves { set; get; }
+    public TemporaryCard[,] tempCard { set; get; }
 
-    Vector3 v3out = Vector3.zero;
- 
+    TemporaryCard selectedTempCard;
+
+    int selectx = -1;
+    int selecty = -1;
 
     public static bool Player1Turn = true;
 
@@ -23,28 +26,131 @@ public class TurnBased : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        coloring();
+        updateselection();
+        move();
     }
 
-
-    void OnMouseDown()
+    void coloring()
     {
-        if(Players.Length == 2)
+        if (Players.Length == 2)
         {
             if (Player1Turn)
             {
                 Players[0].GetComponent<MeshRenderer>().material.color = Color.red;
                 Players[1].GetComponent<MeshRenderer>().material.color = Color.white;
-                Player1Turn = false;
             }
             else
             {
                 Players[1].GetComponent<MeshRenderer>().material.color = Color.red;
                 Players[0].GetComponent<MeshRenderer>().material.color = Color.white;
-                Player1Turn = true;
             }
 
-            Debug.Log("Mouse is over GameObject. " + gameObject.name);
+           // Debug.Log("Mouse is over GameObject. " + gameObject.name);
         }
+    }
+
+    #region Skit
+    public void Selection(GameObject clone)
+    {
+
+        if (selectedTempCard == null)
+        {
+           // ChoseCard(selectx, selecty,clone);
+        }
+
+    }
+
+    void move()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if (selectedTempCard == null)
+            {
+                print("träffade inget kort");
+                return;
+            }
+            else
+            {
+                moveCard(selectx, selecty);
+            }
+        }
+    }
+
+
+    void updateselection()
+    {
+
+        //if (!FindObjectOfType<GameObject>().tag("EndTurnButton") == null)
+        //   return;
+        //print("selectx = " + selectx);
+        //print("selecty = " + selectx);
+        selectx = (int)Input.mousePosition.x;
+        selecty = (int)Input.mousePosition.y;
+        //RaycastHit hit;
+        //if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25f))
+        //{
+        //    Debug.Log(hit.point);
+        //    selectx = (int)hit.point.x;
+        //    selecty = (int)hit.point.z;
+        //    print(new Vector3(selectx, 0, selecty));
+        //}
+        //else
+        //{
+        //    selectx = -1;
+        //    selecty = -1;
+        //}
+    }
+
+    //void ChoseCard(int x, int y,GameObject clone)
+    //{
+    //    if (tempCard[x, y] == null)
+    //    {
+    //      //  tempCard = clone;
+    //    }
+
+    //    if (tempCard[x, y].IsPlayer1 != Player1Turn)
+    //        return;
+
+    //    allowedMoves = tempCard[x, y].PossibleMove();
+
+    //    selectedTempCard = tempCard[x, y];
+    //    HighLight.Instance.HighLightAllowedMoves(allowedMoves);
+
+    //}
+
+    void moveCard(int x, int y)
+    {
+        if (allowedMoves[x, y])
+        {
+            TemporaryCard c = tempCard[x, y];
+
+            if (c != null && c.IsPlayer1 != Player1Turn)
+            {
+                if (c.GetType() == typeof(TemporaryCard))
+                {
+                    print("najs");
+                    return;
+                }
+
+                //activechessman.Remove(c.gameObject);
+                //Destroy(c.gameObject);
+            }
+
+            tempCard[selectedTempCard.CurrentX, selectedTempCard.CurrentY] = null;
+            selectedTempCard.transform.position = transform.position;
+            selectedTempCard.SetPosition(x, y);
+            tempCard[x, y] = selectedTempCard;
+        }
+
+        //HighLight.Instance.HidehighLigst();
+
+        selectedTempCard = null;
+    }
+    #endregion
+
+    void OnMouseDown()
+    {
+        Player1Turn = !Player1Turn;
     }
 }
